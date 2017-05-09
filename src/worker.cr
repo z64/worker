9df6aa25a108b0bb8eeac7137e8550d7d26997f8
z64 <zachnowicki@gmail.com>
2017-05-09 19:49:15 +0000

    remove State, have Pool#handle return Future, guarentee T

--- src/worker.cr
+++ src/worker.cr
@@ -6,12 +6,12 @@ module Worker
     @fiber : Fiber?
 
     # Channel used to send data in and out of this unit
-    @channel : Channel({State, T?, R?})
+    @channel : Channel({T, R?, Exception?})
 
     # Whether this unit is running or not
     getter running = false
 
-    def initialize(@channel, &block : T? -> R?)
+    def initialize(@channel, &block : T -> R?)
       @block = block
     end
 
@@ -23,13 +23,11 @@ module Worker
         loop do
           input = @channel.receive
 
-          next if input[0] == State::Stop
-
           begin
-            output = @block.call input[1].as(T)
-            @channel.send({State::Ok, input[1], output})
-          rescue
-            @channel.send({State::Error, input[1], nil})
+            output = @block.call input[0].as(T)
+            @channel.send({input[0], output, nil})
+          rescue ex
+            @channel.send({input[0], nil, ex})
           end
         end
       end
@@ -49,11 +47,11 @@ module Worker
     @units = [] of Unit(T, R)
 
     # The channel used to communicate to units in this pool
-    @channel = Channel({State, T?, R?}).new
+    @channel = Channel({T, R?, Exception?}).new
 
     # Instances a new Pool with `count` to perform action `&block`.
     # All units will be ran unless `run` is false.
-    def initialize(count : Int32, run : Bool = true, &block : T? -> R?)
+    def initialize(count : Int32, run : Bool = true, &block : T -> R?)
       @unit = Unit(T, R).new(@channel, &block)
 
       count.times { add_unit }
@@ -75,7 +73,7 @@ module Worker
 
     # Send something to the Unit pool.
     def send(obj : T)
-      @channel.send({State::Ok, obj, nil})
+      @channel.send({obj, nil, nil})
     end
 
     # Block until something is received from the pool.
@@ -85,13 +83,15 @@ module Worker
 
     # Helper method for sending something, and then immediately blocking until a result is returned.
     def handle(obj : T)
-      send obj
-      receive
+      future do
+        send obj
+        receive
+      end
     end
 
     # Stops all units in the pool.
     def stop!
-      @units.size.times { @channel.send({State::Stop, nil, nil}) }
+       # @units.size.times { @channel.send({State::Stop, nil, nil}) }
     end
   end
 end
